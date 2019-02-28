94ae8a8f3f6f4a2651be4ffd16ccbf498f77e59d
alextrigue <100306255@alumnos.uc3m.es>
2019-02-28 12:21:42 +0100

    Botones de la pantalla inicial creados y sus actividades: MyRides, NewRides, Profile

--- app/src/main/java/com/g9/letsmoveapp/MainActivity.java
+++ app/src/main/java/com/g9/letsmoveapp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.g9.letsmoveapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startActivityMyRides(View view) {
+        Log.d(LOG_TAG, "MyRides Activity started!!");
+        Intent intent = new Intent(this, MyRides.class);
+        startActivity(intent);
+    }
+
+    public void startActivityNewRides(View view) {
+        Log.d(LOG_TAG, "NewRides Activity started!!");
+        Intent intent = new Intent(this, NewRide.class);
+        startActivity(intent);
+    }
+
+    public void startActivityProfile(View view) {
+        Log.d(LOG_TAG, "Profile Activity started!!");
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
